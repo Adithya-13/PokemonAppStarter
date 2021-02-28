3ebf375981ba38af9817f083a32a29afec90111d
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-02-28 11:46:10 +0700

    menu item config

--- app/src/main/java/com/codext/project/pokemonappstarter/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/codext/project/pokemonappstarter/favorite/FavoriteActivity.kt
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import com.codext.project.pokemonappstarter.databinding.ActivityFavoriteBinding
 
 class FavoriteActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityFavoriteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFavoriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    
+    override fun onNavigateUp(): Boolean {
+        onBackPressed()
+        return false
     }
 }
--- app/src/main/java/com/codext/project/pokemonappstarter/home/HomeActivity.kt
+++ app/src/main/java/com/codext/project/pokemonappstarter/home/HomeActivity.kt
@@ -1,14 +1,38 @@
 package com.codext.project.pokemonappstarter.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.codext.project.pokemonappstarter.R
 import com.codext.project.pokemonappstarter.databinding.ActivityHomeBinding
+import com.codext.project.pokemonappstarter.favorite.FavoriteActivity
 
 class HomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favButton -> goToFavoriteActivity()
+        }
+        return true
+    }
+
+    private fun goToFavoriteActivity() {
+        val intent = Intent(this, FavoriteActivity::class.java)
+        startActivity(intent)
     }
 }
